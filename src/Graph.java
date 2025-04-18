ac5e8b16de8d5e801fde84172e1ba467efb8b2f1
farouk36 <faroukashraf36@gmail.com>
2025-04-18 18:19:16 +0200

    implement floydWarshall

--- src/Graph.java
+++ src/Graph.java
@@ -53,17 +53,48 @@ public class Graph {
         return true;
     }
 
+    private void initializeFloyd(int[][] costs, int[][] predecessors) {
+        for (int i = 0; i < nodes; i++) {
+            Arrays.fill(costs[i], Integer.MAX_VALUE);
+            Arrays.fill(predecessors[i], -1);
+            costs[i][i] = 0;
+        }
+        for (Edge e : edges) {
+            if (e.from == e.to) continue;
+            costs[e.from][e.to] = Math.min(costs[e.from][e.to], e.weight);
+        }
+        for (int i = 0; i < nodes; i++) {
+            for (int j = 0; j < nodes; j++) {
+                if (i != j && costs[i][j] != Integer.MAX_VALUE)
+                    predecessors[i][j] = i;
+            }
+        }
+    }
 
     public boolean floydWarshall(int[][] costs, int[][] predecessors) {
+
+        initializeFloyd(costs,predecessors);
+
+        for (int k = 0; k < nodes ; k++) {
+            for (int from = 0; from < nodes; from++) {
+                for (int to= 0; to < nodes; to++) {
+                    if(costs[from][k]==Integer.MAX_VALUE ||costs[k][to]==Integer.MAX_VALUE)continue;
+
+                    int relax=costs[from][k]+costs[k][to];
+                    if (relax < costs[from][to]) {
+                        costs[from][to] = relax;
+                        predecessors[from][to] = predecessors[k][to];
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < nodes; i++)
+            if(costs[i][i]<0)return false;
         return true;
     }
 
     public static void main(String[] args) throws IOException {
         Graph g=new Graph("input");
-        System.out.println(g.nodes);
-        for(Edge e:g.edges){
-            System.out.println(e.from+" "+e.to+" "+e.weight);
-        }
 
     }
 }
